7982e8b50239510844ac919ae58af6351501adc8
DevMabbott <devmabbott@live.com>
2014-02-11 00:56:28 -0600

    Problem 13

---
+++ eProblem13.cpp
@@ -0,0 +1,59 @@
+/*
+The following iterative sequence is defined for the set of positive integers:
+
+n -> n/2 (n is even)
+n -> 3n + 1 (n is odd)
+
+Using the rule above and starting with 13, we generate the following sequence:
+13 -> 40 -> 20 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1
+
+It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+
+Which starting number, under one million, produces the longest chain?
+
+NOTE: Once the chain starts the terms are allowed to go above one million.
+*/
+
+//BRUTE FORCE
+
+#include <iostream>
+
+int main()
+{
+	
+	unsigned long long maxCount[2] = {0, 0};
+	
+	for(int c = 999999; c > 1; c --)
+	{
+		unsigned long long n = c;
+		unsigned long long count = 0;
+	
+		while ( n > 1)
+		{
+			if (n % 2) 
+			{
+				n = 3 * n + 1;
+				
+			}
+		
+			else
+			{
+				n = n / 2;
+				
+			}
+			
+			count ++;
+		}
+		
+		if ( count > maxCount[1])
+		{	
+			maxCount[1] = count;
+			maxCount[0] = c; 
+		}
+	
+		
+	}
+		
+	std::cout<<"Number with the longest chain is " << maxCount[0] << " chain length = " << maxCount[1];
+
+}	
